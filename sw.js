71a9a9adb8c66463ccc7af90eb483bf8c49deca8
myw4371-a11y <myw4371@gmail.com>
2026-01-07 22:16:14 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
+const cacheName = 'manager-v1';
+const assets = ['./', './index.html', './style.css', './manifest.json'];
 
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
